22f314f57ab06184ec9f48fb8a4ac45994dfab33
JonesCarino <45086782+JonesCarino@users.noreply.github.com>
2018-11-20 10:24:26 +0800

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,39 @@
+package pkg2d.array;
+import java.util.Scanner;
+public class Array {
+
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+     int rows;
+     int columns;
+     
+      
+      System.out.println("Enter number of rows: ");
+            rows = sc.nextInt();
+      System.out.println("Now enter number of columns:");
+            columns = sc.nextInt();
+          
+            int[][]matrix = new int[rows][columns];
+             
+            
+           for(int i = 0; i < rows; i++){
+                    for (int j = 0; j < columns; j++){   
+                    System.out.println("Enter the number for row" + i +"and column" + j +":");
+          
+           matrix [i][j] = sc.nextInt();
+            
+           System.out.println("Array:");
+                for(i = 0; i < rows; i++){
+                   for(j = 0; j < columns; j++){
+                   System.out.print(matrix [i][j] + "\t");
+                   
+               }
+             System.out.println("\t");
+           }
+           }
+        }   
+    }
+ }   
+
